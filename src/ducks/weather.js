e11eefca497f650f06bacef947c78c0a20d82ce8
loren pabst <loren pabst>
2017-06-15 10:52:35 -0600

    added api functions

--- src/ducks/weather.js
+++ src/ducks/weather.js
@@ -1,4 +1,5 @@
 import { buildURL, formatWeatherData } from '../utils/weatherUtils';
+import axios from 'axios';
 
 const initialState = {
   error: false,
@@ -8,14 +9,53 @@ const initialState = {
 };
 
 const RESET = "RESET";
+const SET_WEATHER = 'SET_WEATHER';
+const SET_WEATHER_PENDING = 'SET_WEATHER_PENDING';
+const SET_WEATHER_FULFILLED = 'SET_WEATHER_FULFILLED';
+const SET_WEATHER_REJECTED = 'SET_WEATHER_REJECTED';
 
 export default function weather( state = initialState, action ) {
   switch ( action.type ) {
-    case RESET: return initialState;
-    default: return state;
+    case RESET: 
+      return initialState;
+    case SET_WEATHER_PENDING:
+      return {
+        error: false,
+        loading: true,
+        search: false,
+        weather: {}
+      };
+    case SET_WEATHER_FULFILLED:
+      return {
+        error: false,
+        loading: false,
+        search: false,
+        weather: action.payload
+      };
+    case SET_WEATHER_REJECTED:
+      return {
+        error: true,
+        loading: false,
+        search: false,
+        weather: {}
+      };
+    default: 
+      return state;
   }
 }
 
 export function reset() {
   return { type: RESET };
+}
+
+export function setWeather(location){
+  // var URL = buildURL(location);
+  var URL = 'api.openweathermap.org/data/2.5/weather?q=provo&appid=a4de2f635bec41b2f05fca35f319ee33';
+  var promise = axios.get( URL ).then(res => {
+    formatWeatherData(res.data);
+  })
+  return {
+    type: SET_WEATHER,
+    payload: promise
+  }
 }
--- src/store.js
+++ src/store.js
@@ -1,5 +1,6 @@
-import { createStore } from "redux";
+import { createStore, applyMiddlware } from "redux";
+import promiseMiddleware from 'redux-promise-middleware';
 
 import weather from "./ducks/weather";
 
-export default createStore( weather );
+export default createStore( weather, undefined, applyMiddleware( promiseMiddleware() ) );
